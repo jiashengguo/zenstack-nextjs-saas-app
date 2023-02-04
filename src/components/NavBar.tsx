55b76cc0647ef8124e06d5513736c4adb8022bf2
JG <jiashengguo@outlook.com>
2023-02-04 09:40:40 +0000

    fix: fix logout click handler

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,7 +15,7 @@ export default function NavBar({ user, space }: Props) {
   };
 
   return (
-    <div className="navbar bg-base-100 border-b px-8 py-2">
+    <div className="navbar border-b bg-base-100 px-8 py-2">
       <div className="flex-1">
         <Link href="/">
           <div className="ml-2 hidden text-xl font-semibold text-slate-700 md:inline-block">
@@ -24,19 +24,19 @@ export default function NavBar({ user, space }: Props) {
         </Link>
       </div>
       <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="dropdown-end dropdown">
+          <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
             {user && <Avatar user={user} />}
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow"
+            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
           >
             <li className="border-b border-gray-200">
               {user && <div>{user.name || user.email}</div>}
             </li>
             <li>
-              <a onClick={void onSignout}>Logout</a>
+              <a onClick={() => void onSignout()}>Logout</a>
             </li>
           </ul>
         </div>
